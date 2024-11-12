e4ef4fb8ce880d5447a0a2d624ee5a89fff33efe
nahidulhasan4 <nahidhsan4836@gmail.com>
2024-11-12 23:18:35 +0600

    here some dat

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,8 +9,14 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+    </Head>
     <main className={`${montserrat.variable} font-mont`}>
       <Component {...pageProps} />
     </main>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,12 @@ module.exports = {
         mont: ['var(--font-inter)'],
         
       },
+      color:{
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+      },
     },
   },
   plugins: [],
