b4950156ab51d540060462a9b9b1a640b87713ea
nahidulhasan4 <nahidhsan4836@gmail.com>
2024-11-21 20:09:17 +0600

    here naber design

--- src/component/Navber.js
+++ src/component/Navber.js
@@ -1,12 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
 import Logo from '@/component/Logo'
+import { useRouter } from 'next/router'
 
 
 const CastomLink= ({href, title, className=""}) =>{
+const router = useRouter();
+console.log(router)
+
   return(
-    <Link href={href} className={`${className}`}>
+    <Link href={href} className={`${className} relative group`}>
       {title}
+    <span className={`h-[1px] inline-block  bg-dark absolute left-0 bottom-0.5 group-hover:w-full transition-[width] ease duration-300  ${router.asPath===href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
+
     </Link>
   )
 }
