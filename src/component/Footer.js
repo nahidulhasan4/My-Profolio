0439bfceedacf4bf5e01fa79f1d0beebe635cb3e
nahidulhasan4 <nahidhasan4836@gmail.com>
2024-11-26 18:20:12 +0600

    here compleated footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,7 +6,9 @@ const Footer = () => {
     <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg '>
         <Layout className='py-8 flex items-center justify-between'>
             <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-            <Link href="">Nahid hasn</Link>
+            <div className='flex items-center'>Build With <span className='text-primary  text-2xl px-1'>&#9825;</span>by &nbsp;
+            <Link href="https://github.com/nahidulhasan4">Nahid hasn</Link>
+            </div>
             <Link href="">Say Help</Link>
         </Layout>
     </footer>
