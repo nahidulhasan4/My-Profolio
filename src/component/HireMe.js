83b59e44c0c8486cabc2e404a8c63ffe7713cfd0
nahidulhasan4 <nahidhasan4836@gmail.com>
2024-11-30 20:37:16 +0600

    here add carcal

--- src/component/HireMe.js
+++ src/component/HireMe.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import {CircularText} from './Icons'
 const HireMe = () => {
   return (
     <div className=' left-4 bottom-4 flex items-center justify-center overflow-hidden'>
         <div className='w-48 h-auto flex items-cneter justify-center relative'>
-
+        <CircularText />
         </div>
         
     </div>
