3605b2edb9258b02249d7710f8fb7d7bcf739d7a
nahidulhasan4 <nahidhasan4836@gmail.com>
2024-12-05 23:06:56 +0600

    here some data

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -4,7 +4,7 @@ const AboutMe = () => {
   return (
     <div className=" fixed left-4 bottom-4 flex items-center justify-center overflow-hidden">
     <div className="w-48 h-auto flex items-cneter justify-center relative">
-      <CircularText className={"fill-dark animate-spin-slow"} />
+      
       <Link
         href="mailto:nahidhasan4836@gmail.com"
         className="flex items-center justify-center absolute right-1/2  top-[68px] translate-x-1/2 translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-auto rounded "
