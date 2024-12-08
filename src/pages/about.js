99888cf1105a6d0131bb9677008f55b17cdfc8e3
nahidulhasan4 <nahidhasan4836@gmail.com>
2024-12-08 23:44:57 +0600

    here some data

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,6 +15,34 @@ const about = () => {
       <main className="flex w-full flex-col items-center justify-center">
         <Layout className="pt-16">
           <AnimatedText text="Passion Fules Purpose!" />
+          <div className=" grid w-full grid-col-8 gap-16  ">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h1 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biograpy
+              </h1>
+              <p className="">
+                Hi, I'm CodeBucks, a web developer and UI/UX designer with a
+                passion for creating beautiful, functional, and user-centered
+                digital experiences. With 4 years of experience in the field. I
+                am always looking for new and innovative ways to bring my
+                clients' visions to life.
+              </p>
+              <p>
+                {" "}
+                - I believe that design is about more than just making things
+                look pretty – it's about solving problems and creating
+                intuitive, enjoyable experiences for users.{" "}
+              </p>
+
+              <p>
+                - Whether I'm working on a website, mobile app, or other digital
+                product, I bring my commitment to design excellence and
+                user-centered thinking to every project I work on. I look
+                forward to the opportunity to bring my skills and passion to
+                your next project.
+              </p>
+            </div>
+          </div>
         </Layout>
       </main>
     </>
