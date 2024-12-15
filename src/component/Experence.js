899b470bb6699ee90e18b14c4de1e925cdc849b7
nahidulhasan4 <nahidhasan4836@gmail.com>
2024-12-15 22:43:10 +0600

    here some data

--- src/component/Experence.js
+++ src/component/Experence.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
+
+const Details =({positon,company,companyLInk,time,adress,work})=>{
+
+}
+
+
 const Experence = () => {
   return (
     <div className='my-64'>
-        <h2 className='font-bold text-8xl mb-32 w-full text-center'></h2>
+        <h2 className='font-bold text-8xl mb-32 w-full text-center'>
         Experence
+        </h2>
+       <div className='w-[75%] mx-auto relative'>
+       <ul>
+          <li>
+
+          </li>
+        </ul>
+       </div>
+
+
     </div>
   )
 }
