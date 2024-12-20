daff11f5ee29d79dfad8cc17431a49ae4f340747
nahidulhasan4 <nahidhasan4836@gmail.com>
2024-12-20 21:51:59 +0600

    here some data

--- src/component/Experence.js
+++ src/component/Experence.js
@@ -2,9 +2,13 @@ import {React, useRef}from "react";
 
 
 import {motion, useScroll} from "framer-motion";
+import LiIcon from "./LiIcon";
 const Details = ({ positon, company, companyLInk, time, adress, work }) => {
   return (
+
+   
     <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+       <LiIcon />
       <div>
         <h3 className="capitalize font-bold text-2xl">
           {positon} &nbsp;{" "}
---
+++ src/component/LiIcon.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LiIcon = () => {
+  return (
+    <figure>
+        <svg width="75" height="75" viewBox='0 0 100 100 '>
+            <circal cx="75" cy="50" r="20" />
+            <circal  cx="75" cy="50" r="20"/>
+            <circal  cx="75" cy="50" r="20"/>
+        </svg>
+    </figure>
+  )
+}
+
+export default LiIcon
