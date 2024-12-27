d00c14e1b8833a813bd7937a0b91d302a1c81d8f
nahidulhasan4 <nahidhasan4836@gmail.com>
2024-12-28 01:09:44 +0600

    here add dark mode hooks

--- src/component/Logo.js
+++ src/component/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
     <div className=" flex items-center justify-center mt-2  ">
       <MotionLink
         href="/"
-        className='w-16 h-16 bg-dark py-2  text-light flex items-center rounded-full justify-center text-2xl'
+        className='w-16 h-16 bg-dark py-2  text-light flex items-center border border-solid border-transparent dark:border-light rounded-full justify-center text-2xl'
         whileHover={{
           
         backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
--- src/component/Navber.js
+++ src/component/Navber.js
@@ -2,8 +2,9 @@ import Link from 'next/link'
 import React from 'react'
 import Logo from '@/component/Logo'
 import { useRouter } from 'next/router'
-import {TwitterIcon,GithubIcon, LinkedInIcon, PinterestIcon, DribbbleIcon} from '@/component/Icons'
+import {TwitterIcon,GithubIcon, LinkedInIcon, PinterestIcon, DribbbleIcon, SunIcon, MoonIcon} from '@/component/Icons'
 import{motion}from 'framer-motion'
+import useThemeSwticher from './hooks/useThemeSwticher'
 
 const CastomLink= ({href, title, className=""}) =>{
 const router = useRouter();
@@ -12,15 +13,17 @@ console.log(router)
   return(
     <Link href={href} className={`${className} relative group`}>
       {title}
-    <span className={`h-[1px] inline-block  bg-dark absolute left-0 bottom-0.5 group-hover:w-full transition-[width] ease duration-300  ${router.asPath===href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
+    <span className={`h-[1px] inline-block  bg-dark absolute left-0 bottom-0.5 group-hover:w-full transition-[width] ease duration-300  ${router.asPath===href ? 'w-full' : 'w-0'} dark:bg-light`}>&nbsp;</span>
 
     </Link>
   )
 }
 const Navber = () => {
+  const [mode, setMode] = useThemeSwticher();
+
   return (
   <>
-  <header className='w-full px-32 py-8 font-medium flex items-center justify-between '>
+  <header className='w-full px-32 py-8 font-medium dark:text-light flex items-center justify-between '>
     <nav className='px-3'>
         <CastomLink href="/" title="Home" className="mr-4"/> 
         <CastomLink href="/about" title="About" className="mx-4" />
@@ -64,6 +67,14 @@ const Navber = () => {
         >
         <DribbbleIcon />
         </motion.a>
+    <button onClick={() => setMode(mode === "light" ? "dark" : "light") } 
+      className={`ml-3 flex items-center justify-center  rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
+      >
+{
+  mode === "dark" ? <SunIcon className={"fill-dark"}/> : <MoonIcon className={"fill-dark"}/>
+}
+    </button>
+
     </nav>
      <div className=' absolute left-[50%] top-2 translate-x-[50%]'>
      <Logo />
